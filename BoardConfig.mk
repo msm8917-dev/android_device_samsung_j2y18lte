408bc98be8badc5c52b8afb76e992470d9f79060
Gabriel2392 <gabriel824m@gmail.com>
2022-11-09 19:35:00 -0300

    Build vendor inside system

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,6 @@ TARGET_KERNEL_CONFIG := j4primelte_defconfig
 
 # Filesystem
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2453520000
-BOARD_VENDORIMAGE_PARTITION_SIZE := 295698432
 
 # Inherit from common msm8917-common
 -include device/samsung/msm8917-common/BoardConfigCommon.mk
