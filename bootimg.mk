514a878c00278df12bc695d5b7cc3c5f0d83224d
roynatech2544 <whiteshell2544@naver.com>
2022-11-19 16:42:28 +0900

    j2y18lte: Include dtbtool

--- bootimg.mk
+++ bootimg.mk
@@ -1,6 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
+CUSTOM_DTB_TOOL := $(LOCAL_PATH)/dtbTool
+KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
+INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
+
+$(INSTALLED_DTIMAGE_TARGET): $(CUSTOM_DTB_TOOL) $(KERNEL_OUT)
+	@echo ----- Making dt image ------
+	$(hide) $(CUSTOM_DTB_TOOL) -s $(BOARD_KERNEL_PAGESIZE) -o $(INSTALLED_DTIMAGE_TARGET) -p $(KERNEL_OUT)/scripts/dtc $(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/dts
+	@echo ----- Made dt image: $@ --------
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) $(recovery_kernel) $(recovery_ramdisk)
 	@echo -e ${CL_GRN}"----- Making recovery image ------"${CL_RST}
