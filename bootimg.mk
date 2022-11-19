da816d9d9f218f27676f38e48cb3d0d8501fa34b
roynatech2544 <whiteshell2544@naver.com>
2022-11-19 17:48:08 +0900

    j2y18lte: bootimgmk: Output the file needed for mkbootimg

--- bootimg.mk
+++ bootimg.mk
@@ -3,11 +3,12 @@ LOCAL_PATH := $(call my-dir)
 FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
 CUSTOM_DTB_TOOL := $(LOCAL_PATH)/dtbTool
 KERNEL_OUT := $(PRODUCT_OUT)/kernel
-INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dtb.img
+INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
 $(INSTALLED_DTIMAGE_TARGET): $(CUSTOM_DTB_TOOL) $(KERNEL_OUT)
 	@echo ----- Making dt image ------
 	$(hide) $(CUSTOM_DTB_TOOL) -s $(BOARD_KERNEL_PAGESIZE) -o $(INSTALLED_DTIMAGE_TARGET) -p $(KERNEL_OUT)/scripts/dtc $(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/dts/samsung/msm8917
+	$(hide) mv $(PRODUCT_OUT)/dt.img $(PRODUCT_OUT)/dtb.img
 	@echo ----- Made dt image: $@ --------
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) $(recovery_kernel) $(recovery_ramdisk)
