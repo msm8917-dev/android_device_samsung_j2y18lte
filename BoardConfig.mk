53d3a1a72b3b187fd85ed76be923b80c04b8a505
roynatech2544 <whiteshell2544@naver.com>
2022-11-28 22:03:21 +0900

    j2y18lte: Remove DTB_IN_BOOTIMG
    Custom bootimg mk covering this

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,8 @@ BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 BOARD_KERNEL_IMAGE_NAME := zImage
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+# Custom bootimg.mk covering this - legacy --dt  not --dtb
+# BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := j2y18lte_defconfig
 #TARGET_KERNEL_SOURCE := kernel/samsung/msm8917
