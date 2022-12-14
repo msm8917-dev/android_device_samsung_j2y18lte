98300238b189482ac5bd75f837319047a9772c08
roynatech2544 <whiteshell2544@naver.com>
2022-12-14 17:35:56 +0900

    j2y18lte: Update build fingerprint

--- lineage_j2y18lte.mk
+++ lineage_j2y18lte.mk
@@ -34,7 +34,7 @@ PRODUCT_MODEL := Galaxy J2 Pro
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 TARGET_VENDOR := samsung
 TARGET_VENDOR_PRODUCT_NAME := j2y18lte
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="j2y18lteks-user 7.1.1 NMF26X.J250NKOU1ARD1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="j2y18ltekx-user 7.1.1 NMF26X.J250NKOU1ARL1 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "samsung/j2y18lteks/j2y18lte:7.1.1/NMF26X.J250NKOU1ARD1:user/release-keys"
+BUILD_FINGERPRINT := "samsung/j2y18ltekx/j2y18ltekx:7.1.1/NMF26X/J250NKOU1ARL1:user/release-keys"
